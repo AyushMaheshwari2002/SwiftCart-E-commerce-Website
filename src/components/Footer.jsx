ebbab97aabd30897736fa3551ced264e7ea7ad4b
Ayush <ayushmaheshwari07062002@gmail.com>
2024-01-12 15:39:21 +0530

    done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,101 @@
+import './Footer.css'
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">Made by {" "}
+              <a  href="https://github.com/AyushMaheshwari2002" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Ayush Maheshwari</a>
+            </p>
+            <a className="text-dark fs-4" href="https://github.com/AyushMaheshwari2002" target="_blank" rel="noreferrer">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+        <div className="footer__container">
+                <div className="footer__items__container">
+                    <div className="footer__help__container">
+                        <div className="footer__help__header">
+                            <h1>Help</h1>
+                        </div>
+                        <ul className="fotter__help__links">
+                            <li className="help__link">
+                                Shipping
+                            </li>
+                            <li className="help__link">
+                                Refund
+                            </li>
+                            <li className="help__link">
+                                FAQ
+                            </li>
+                            <li className="help__link">
+                                Accessiblity
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer__contact__container">
+                        <div className="footer__contact__header">
+                            <h1>Contact Us</h1>
+                        </div>
+                        <ul className="footer__contacts">
+                            <li className="footer__contact">
+                                <LocalPhoneIcon /> <span>+123 4567 890</span>
+                            </li>
+                            <li className="footer__contact">
+                                <EmailIcon /> <span>shop@SwiftCart.com</span>
+                            </li>
+                            <li className="footer__contact">
+                                <LocationOnIcon /> <span>Addis Ababa, Ethiopia</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer__social__link__container">
+                        <div className="footer__social__link__header">
+                            <h1>Stay Connected</h1>
+                        </div>
+                    <ul className="footer__social__links">
+                            <li className="social__link">
+                                <TwitterIcon />
+                            </li>
+                            <li className="social__link">
+                                <InstagramIcon />
+                            </li>
+                            <li className="social__link">
+                                <YouTubeIcon />
+                            </li>
+                            <li className="social__link">
+                                <TelegramIcon />
+                            </li>
+                            <li className="social__link">
+                                <PinterestIcon />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="fotter__copyright__container">
+                    <ul className='nav'>
+                        <li className="footer__copyright">©2022 Shema Ltd. |</li>
+                        <li className="footer__terms__condition"> | Terms & Condition |</li>
+                        <li className="footer__privacy__policy">| Privacy Policy</li>
+                    </ul>
+                     </div>
+            </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
+
+
